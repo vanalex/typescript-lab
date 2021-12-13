9747d893caa224231e0a9c18e0ea4e496d050f93
alex <alex.pajaron@vier.ai>
2021-12-13 21:33:35 +0100

    add union and type guard example

--- primitive_types.ts
+++ primitive_types.ts
@@ -24,3 +24,36 @@ function printObject(obj: string | number) {
 
 printObject(1);
 printObject("string value");
+
+//
+// type guards
+//
+
+function addWithUnion(
+    arg1: string | number,
+    arg2: string | number
+) {
+    // return arg1 + arg2;
+}
+
+function addWithTypeGuard(
+    arg1: string | number,
+    arg2: string | number
+) {
+    if (typeof arg1 === "string") {
+        // arg 1 is treated as a string
+        console.log(`arg1 is of type string`);
+        return arg1 + arg2;
+    }
+    if (typeof arg1 === "number" && typeof arg2 === "number") {
+        // both are numbers
+        console.log(`arg1 and arg2 are numbers`);
+        return arg1 + arg2;
+    }
+    console.log(`default return treat both as strings`)
+    return arg1.toString() + arg2.toString();
+}
+
+console.log(` "1", "2" = ${addWithTypeGuard("1", "2")}`);
+console.log(`  1 ,  2  = ${addWithTypeGuard(1, 2)}`);
+console.log(`  1 , "2" = ${addWithTypeGuard(1, "2")}`);
